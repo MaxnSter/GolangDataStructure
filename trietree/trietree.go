b9d787734df09c8d9c6c30c694b0e96d14a40d1b
maxnster <maxnster20@gmail.com>
2018-04-07 16:58:19 +0800

    词典树

---
+++ trietree/trietree.go
@@ -0,0 +1,120 @@
+package trietree
+
+import (
+	"bufio"
+	"errors"
+	"io"
+)
+
+type trieNode struct {
+	isEnd      bool
+	childNodes map[byte]*trieNode
+}
+
+type trieTree struct {
+	root *trieNode
+}
+
+func NewTrieTree() *trieTree {
+	return &trieTree{
+		root: &trieNode{
+			isEnd:      false,
+			childNodes: make(map[byte]*trieNode),
+		}}
+}
+
+func BuildTrieTree(rd io.Reader) *trieTree {
+	t := NewTrieTree()
+	scan := bufio.NewScanner(rd)
+	scan.Split(bufio.ScanWords)
+
+	for scan.Scan() {
+		t.AddWord(scan.Text())
+	}
+
+	return t
+}
+
+func (t *trieTree) AddWord(word string) error {
+	if len(word) == 0 {
+		return errors.New("empty word")
+	}
+
+	tmp := t.root
+	for i := 0; i < len(word); i++ {
+		b := word[i]
+
+		if _, ok := tmp.childNodes[b]; !ok {
+			tmp.childNodes[b] = &trieNode{
+				isEnd:      false,
+				childNodes: make(map[byte]*trieNode),
+			}
+		}
+
+		tmp = tmp.childNodes[b]
+		if i == len(word)-1 {
+			tmp.isEnd = true
+		}
+	}
+
+	return nil
+
+}
+
+func (t *trieTree) Chk(line string) bool {
+	if len(line) == 0 {
+		return false
+	}
+
+	tmp := t.root
+	begin, pos := 0, 0
+
+	for pos < len(line) {
+		b := line[pos]
+
+		if node, ok := tmp.childNodes[b]; !ok {
+			begin++
+			pos = begin
+			tmp = t.root
+		} else if node.isEnd {
+			return true
+		} else {
+			pos++
+			tmp = node
+		}
+	}
+
+	return false
+}
+
+func (t *trieTree) Filter(line string, replace string) (string, error) {
+	if len(line) == 0 {
+		return line, errors.New("empty line")
+	}
+
+	tmp := t.root
+	begin, pos := 0, 0
+	result := make([]byte, 0, len(line))
+
+	for pos < len(line) {
+		b := line[pos]
+
+		if node, ok := tmp.childNodes[b]; !ok {
+			result = append(result, line[begin])
+			begin++
+			pos = begin
+			tmp = t.root
+		} else if node.isEnd {
+			result = append(result, replace...)
+			pos++
+			begin = pos
+			tmp = t.root
+		} else {
+			pos++
+			tmp = node
+		}
+	}
+
+	result = append(result, line[begin:]...)
+	return string(result), nil
+}
---
+++ trietree/trietree_test.go
@@ -0,0 +1,95 @@
+package trietree
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestNewTrieTree(t *testing.T) {
+	tree := NewTrieTree()
+	if tree == nil {
+		t.Fail()
+	}
+}
+
+func TestBuildTrieTree(t *testing.T) {
+	input := []byte("ab edf 贪玩蓝月")
+	rd := bytes.NewReader(input)
+	tree := BuildTrieTree(rd)
+	if tree == nil {
+		t.Fail()
+	}
+}
+
+func TestTrieTree_Chk(t *testing.T) {
+	input := []byte("ab edf 贪玩蓝月")
+	rd := bytes.NewReader(input)
+	tree := BuildTrieTree(rd)
+
+	lines := []string {
+		"我代言了贪玩蓝月",
+		"abcd",
+		"edf",
+		"acccccccab",
+	}
+
+	for _, line := range lines {
+		if !tree.Chk(line) {
+			t.Log("chk error at %s", line)
+			t.Fail()
+		}
+	}
+}
+
+func TestTrieTree_Chk2(t *testing.T) {
+
+	input := []byte("ab edf 贪玩蓝月")
+	rd := bytes.NewReader(input)
+	tree := BuildTrieTree(rd)
+
+	lines := []string {
+		"我代言了贪玩",
+		"helllllo",
+		"deef",
+		"acb",
+	}
+
+	for _, line := range lines {
+		if tree.Chk(line) {
+			t.Log("chk error at ", line)
+			t.Fail()
+		}
+	}
+}
+
+func TestTrieTree_Filter(t *testing.T) {
+
+	input := []byte("ab edf 贪玩蓝月")
+	rd := bytes.NewReader(input)
+	tree := BuildTrieTree(rd)
+
+	lines := []string {
+		"我代言了贪玩蓝月",
+		"abcd",
+		"def",
+		"acccccccab",
+	}
+
+	correct := []string {
+		"我代言了*",
+		"*cd",
+		"def",
+		"accccccc*",
+	}
+
+	for i, line := range lines {
+		result, _ := tree.Filter(line, "*")
+		if strings.Compare(result, correct[i]) != 0 {
+			t.Logf("filter error at %s, should be %s", result, correct[i])
+			t.Fail()
+		}
+	}
+}
+
+
