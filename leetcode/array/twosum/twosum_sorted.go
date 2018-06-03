1b3f66644e1bfc02694ea9a095b5d258ec09108d
maxnster <maxnster20@gmail.com>
2018-06-03 12:57:19 +0800

    add leetcode-twosum

---
+++ leetcode/array/twosum/twosum_sorted.go
@@ -0,0 +1,35 @@
+package twosum
+
+// https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/description/
+func TwoSumSorted(numbers []int, target int) []int {
+	for i := 0; i < len(numbers)-1; i++ {
+		complement := target - numbers[i]
+		if idx := bSearch(numbers, complement); idx != -1 && idx != i {
+			return []int{i + 1, idx + 1}
+		}
+
+	}
+
+	return nil
+}
+
+func bSearch(numbers []int, finder int) (idx int) {
+	begin := 0
+	end := len(numbers) - 1
+
+	for begin <= end {
+		mid := begin + (end-begin)/2
+
+		if numbers[mid] > finder {
+			end = mid - 1
+			continue
+		} else if numbers[mid] < finder {
+			begin = mid + 1
+			continue
+		} else {
+			return mid
+		}
+	}
+
+	return -1
+}
