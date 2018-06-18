eaee419ece9606c1a0c286d5bc560ae9a5b7bc2e
maxnster <maxnster20@gmail.com>
2018-06-18 21:39:54 +0800

    add non_descrising

---
+++ leetcode/array/non_decrsing/non_decrsing_test.go
@@ -0,0 +1,12 @@
+package non_decrsing
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	assert.Equal(t,true, checkPossibility([]int{4,2,3} ))
+	assert.Equal(t,false, checkPossibility([]int{4,2,1} ))
+}
