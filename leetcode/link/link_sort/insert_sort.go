6607575426f911bccda2b56319a46998619f6d27
maxnster <maxnster20@gmail.com>
2018-06-22 20:21:48 +0800

    add insert sort

---
+++ leetcode/link/link_sort/insert_sort.go
@@ -0,0 +1,55 @@
+package link_sort
+
+import "math"
+
+/*
+147. 对链表进行插入排序
+插入排序算法：
+
+插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+重复直到所有输入数据插入完为止。
+
+
+示例 1：
+
+输入: 4->2->1->3
+输出: 1->2->3->4
+示例 2：
+
+输入: -1->5->3->4->0
+输出: -1->0->3->4->5
+*/
+/**
+* Definition for singly-linked list.
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//折腾了好久,烦烦烦
+func insertionSortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	dummyHead := &ListNode{Next: head, Val: math.MinInt64}
+	for endNode := head; endNode.Next != nil; {
+		insertNode, insertNodePrev := endNode.Next, endNode
+
+		hHead := dummyHead
+		for ; hHead != insertNodePrev && hHead.Next.Val < insertNode.Val; hHead = hHead.Next {
+		}
+		//找到第一个可插入点
+		if hHead != insertNodePrev {
+			insertNodePrev.Next = insertNode.Next
+			insertNode.Next = hHead.Next
+			hHead.Next = insertNode
+		} else {
+			endNode = endNode.Next
+		}
+	}
+
+	return dummyHead.Next
+}
---
+++ leetcode/link/link_sort/link_sort_test.go
@@ -0,0 +1,67 @@
+package link_sort
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func dumpList(l *ListNode) {
+	wr := bufio.NewWriter(os.Stdout)
+	for l != nil {
+		if l.Next != nil {
+			wr.WriteString(fmt.Sprintf("%d->", l.Val))
+		} else {
+			wr.WriteString(strconv.Itoa(l.Val))
+		}
+		l = l.Next
+	}
+	wr.WriteByte('\n')
+	wr.Flush()
+}
+
+func makeList(vals ...int) *ListNode {
+	var head *ListNode
+
+	for i := len(vals) - 1; i >= 0; i-- {
+		node := &ListNode{Val: vals[i]}
+		node.Next = head
+		head = node
+	}
+
+	if !listEqual(head, vals...) {
+		panic("makeList error")
+	}
+
+	return head
+}
+
+func listEqual(l *ListNode, vals ...int) bool {
+	head := l
+	for _, val := range vals {
+		if l == nil {
+			fmt.Println("list shortted")
+			dumpList(head)
+			return false
+		}
+
+		if l.Val != val {
+			fmt.Printf("list:%d, should be %d\n", l.Val, val)
+			dumpList(head)
+			return false
+		}
+
+		l = l.Next
+	}
+	return true
+}
+
+func TestInsertSOrt(t *testing.T) {
+	l := makeList(-1,5,3,4,0)
+	ls := insertionSortList(l)
+	assert.Equal(t, true, listEqual(ls,-1,0,3,4,5))
+}
