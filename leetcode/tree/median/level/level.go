7e54ac14e8d3454ddd15f5f9eb4732c1474a3037
maxnster <maxnster20@gmail.com>
2018-06-24 15:16:00 +0800

    add level

--- leetcode/tree/median/level/level.go
+++ leetcode/tree/median/level/level.go
@@ -3,30 +3,28 @@ package level
 import "github.com/MaxnSter/GolangDataStructure/leetcode/queue"
 
 /*
-103. 二叉树的锯齿形层次遍历
+102. 二叉树的层次遍历
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
 
-给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
-
-例如：
-给定二叉树 [3,9,20,null,null,15,7],
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
 
     3
    / \
   9  20
     /  \
    15   7
-返回锯齿形层次遍历如下：
+返回其层次遍历结果：
 
 [
   [3],
-  [20,9],
+  [9,20],
   [15,7]
 ]
+
 */
-
 /**
-* Definition for a binary tree node.
-
+ * Definition for a binary tree node.
  */
 type TreeNode struct {
 	Val   int
@@ -34,14 +32,13 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func zigzagLevelOrder(root *TreeNode) [][]int {
+func levelOrder(root *TreeNode) [][]int {
 	if root == nil {
 		return nil
 	}
 
-	q, result := queue.NewQueue(), make([][]int, 0)
+	q, result := queue.NewQueue(), make([][]int,0)
 	q.PushBack(root)
-	count := 1
 
 	for !q.Empty() {
 		level := make([]int, 0)
@@ -52,28 +49,12 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 			q.PopFront()
 
 			level = append(level, node.Val)
-			if node.Left != nil {
-				q.PushBack(node.Left)
-			}
-			if node.Right != nil {
-				q.PushBack(node.Right)
-			}
+			if node.Left != nil {q.PushBack(node.Left)}
+			if node.Right != nil {q.PushBack(node.Right)}
 		}
 
-		count++
-		if count%2 == 1 {
-			reverse(level)
-		}
 		result = append(result, level)
 	}
-	return result
-}
 
-func reverse(nums []int) {
-	b, e := 0, len(nums)-1
-	for b < e {
-		nums[b], nums[e] = nums[e], nums[b]
-		b++
-		e--
-	}
+	return result
 }
