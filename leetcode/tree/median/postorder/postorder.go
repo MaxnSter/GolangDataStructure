18ef57f097506789b860522d8b270f9c1832d56e
maxnster <maxnster20@gmail.com>
2018-06-25 20:42:09 +0800

    add norecursive

--- leetcode/tree/median/postorder/postorder.go
+++ leetcode/tree/median/postorder/postorder.go
@@ -1,5 +1,7 @@
 package postorder
 
+import "github.com/MaxnSter/GolangDataStructure/leetcode/stack"
+
 /*
 145. 二叉树的后序遍历
 给定一个二叉树，返回它的 后序 遍历。
@@ -39,5 +41,34 @@ func traversal(root *TreeNode, result *[]int) {
 	traversal(root.Left, result)
 	traversal(root.Right, result)
 	*result = append(*result, root.Val)
+}
 
+//非递归,用两个栈实现
+func postorderTraversalNoRecursive(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	s1, s2, result := stack.NewStack(), stack.NewStack(), make([]int, 0)
+	s1.Push(root)
+
+	for !s1.Empty() {
+		node := s1.Top().(*TreeNode)
+		s1.Pop()
+
+		s2.Push(node)
+
+		if node.Left != nil {
+			s1.Push(node.Left)
+		}
+		if node.Right != nil {
+			s1.Push(node.Right)
+		}
+	}
+
+	for !s2.Empty() {
+		result = append(result, s2.Top().(int))
+		s2.Pop()
+	}
+	return result
 }
