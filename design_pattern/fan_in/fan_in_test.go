e510ebd29a6265c4dd6bc11ec7476110b41bba8d
Maxnster <maxnster20@gmail.com>
2019-02-08 22:43:51 +0800

    add more test

--- design_pattern/fan_in/fan_in_test.go
+++ design_pattern/fan_in/fan_in_test.go
@@ -1,8 +1,9 @@
 package fan_in
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFanInWithPicker(t *testing.T) {
@@ -67,6 +68,56 @@ func TestFanInWithPicker(t *testing.T) {
 	out := FanInWithPicker(pickF(), ch1, ch2, ch3, ch4)
 	for data := range out {
 		actual = append(actual, data.(int))
+	}
+
+	assert.EqualValues(t, expect, actual)
+}
+
+func TestFanInWithPicker2(t *testing.T) {
+	// goog....goog
+	var expect []string
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			expect = append(expect, "g")
+			expect = append(expect, "o")
+		} else {
+			expect = append(expect, "o")
+			expect = append(expect, "g")
+		}
+	}
+	sendF := func(ch chan interface{}, val interface{}) {
+		num := 100
+		for i := 0; i < num; i++ {
+			ch <- val
+		}
+		close(ch)
+	}
+
+	ch1 := make(chan interface{})
+	go sendF(ch1, "g")
+
+	ch2 := make(chan interface{})
+	go sendF(ch2, "o")
+
+	i := 0
+	picker := func() OnPick {
+		return func(available []int) []int {
+			defer func() {
+				i++
+			}()
+
+			if i%2 == 0 {
+				return []int{0, 1}
+			} else {
+				return []int{1, 0}
+			}
+		}
+	}
+
+	var actual []string
+	out := FanInWithPicker(picker(), ch1, ch2)
+	for data := range out {
+		actual = append(actual, data.(string))
 	}
 
 	assert.EqualValues(t, expect, actual)
