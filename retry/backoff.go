1f0f3edfcb6e1f3e1117f2c836735cc81f898592
Maxnster <maxnster20@gmail.com>
2019-02-10 10:47:53 +0800

    add ExponentialLimitBackoff

--- retry/backoff.go
+++ retry/backoff.go
@@ -11,11 +11,18 @@ func ConstantBackoff(n int, amount time.Duration) []time.Duration {
 }
 
 func ExponentialBackoff(n int, initialAmount time.Duration) []time.Duration {
+	return ExponentialLimitBackoff(n, initialAmount, -1)
+}
+
+func ExponentialLimitBackoff(n int, initialAmount, maxAmount time.Duration) []time.Duration {
 	ts := make([]time.Duration, n)
 	next := initialAmount
 	for i := 0; i < n; i++ {
 		ts[i] = next
 		next *= 2
+		if maxAmount > 0 && next > maxAmount {
+			next = maxAmount
+		}
 	}
 	return ts
 }
